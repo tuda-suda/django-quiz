7d6d0966b9d6e7067743d10ca8e214c51ee70572
tuda-suda <marat.gumerov99@gmail.com>
2021-05-25 22:34:34 +0500

    Add models

--- quiz_app/models.py
+++ quiz_app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Quiz(models.Model):
+    title = models.CharField(max_length=250)
+
+
+class Question(models.Model):
+    text = models.TextField()
+
+    quiz = models.ForeignKey(
+        Quiz,
+        related_name='questions',
+        on_delete=models.CASCADE
+    )
+
+
+class Choice(models.Model):
+    text = models.TextField()
+    is_correct = models.BooleanField()
+
+    question = models.ForeignKey(
+        Question,
+        related_name='choices',
+        on_delete=models.CASCADE
+    )
